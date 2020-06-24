53907835a28ccee5ede152bfa00a5ef1eaa3407f
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2020-06-24 19:34:54 +0530

    Quotes Crawler

---
+++ 03_Web_Crawler_Using_Scrapy/quotes/quotes/spiders/recursive.py
@@ -0,0 +1,29 @@
+import scrapy
+
+class RecursiveSpider(scrapy.Spider):
+    name = "recur"
+    
+    def start_requests(self):
+        urls = ['http://quotes.toscrape.com/page/1/']
+        
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+            
+    def parse(self, response):
+        
+        for q in response.css("div.quote"):
+            text = q.css('span.text::text').get()
+            author = q.css('small.author::text').get()
+            tags = q.css('a.tag::text').getall()
+               
+            yield {
+                'text': text,
+                'author': author,
+                'tags': tags,
+            }      
+                
+        next_page = response.css('li.next a::attr(href)').get()
+            
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
